a604531efd6e208cfee6ae6704127abafc343568
xuyang-liu16 <liuxuyang@stu.scu.edu.cn>
2026-02-11 02:52:09 -0800

    Move scholar badge logic to external script

---
+++ assets/js/google_scholar_stats.js
@@ -0,0 +1,32 @@
+---
+---
+$(document).ready(function () {
+    {% if site.google_scholar_stats_use_cdn %}
+    var gsDataBaseUrl = 'https://cdn.jsdelivr.net/gh/{{ site.repository }}@'
+    {% else %}
+    var gsDataBaseUrl = 'https://raw.githubusercontent.com/{{ site.repository }}/'
+    {% endif %}
+    $.getJSON(gsDataBaseUrl + "google-scholar-stats/gs_data.json", function (data) {
+        var citationEles = document.getElementsByClassName('show_paper_citations')
+        Array.prototype.forEach.call(citationEles, element => {
+            var paperId = element.getAttribute('data')
+            if (!paperId || !data.publications || !data.publications[paperId]) return;
+            var numCitations = data.publications[paperId].num_citations
+            element.innerHTML = '| Citations: ' + numCitations;
+        });
+        var allCitation = document.getElementsByClassName('all_citation_badges')
+        Array.prototype.forEach.call(allCitation, element => {
+            var numCitations = data.citedby
+            element.href = '{{ site.author.googlescholar }}'
+            element.innerHTML = '<img src="https://img.shields.io/badge/Paper%20Citations-' + numCitations +'-blue?style=social&logo=googlescholar">'
+        });
+        var citationBadges = document.getElementsByClassName('paper_citations_badges')
+        Array.prototype.forEach.call(citationBadges, element => {
+            var paperId = element.getAttribute('data')
+            if (!paperId || !data.publications || !data.publications[paperId]) return;
+            var numCitations = data.publications[paperId].num_citations
+            element.href = 'https://scholar.google.com/citations?view_op=view_citation&citation_for_view=' + paperId;
+            element.innerHTML = '<img src="https://img.shields.io/badge/citations-' + numCitations +'-blue?style=social&logo=googlescholar">'
+        });
+    });
+})
